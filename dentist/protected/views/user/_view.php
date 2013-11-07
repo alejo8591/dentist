295091dd9e0e1e371b8f511642425d448992197e
Alejandro Romero <alejo8591@gmail.com>
2013-11-06 20:11:23 -0500

    update CRUD files for User model

---
+++ dentist/protected/views/user/_view.php
@@ -0,0 +1,59 @@
+<?php
+/* @var $this UserController */
+/* @var $data User */
+?>
+
+<div class="view">
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('username')); ?>:</b>
+	<?php echo CHtml::link(CHtml::encode($data->username), array('view', 'id'=>$data->username)); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('type_document')); ?>:</b>
+	<?php echo CHtml::encode($data->type_document); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('user_status')); ?>:</b>
+	<?php echo CHtml::encode($data->user_status); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('type_user')); ?>:</b>
+	<?php echo CHtml::encode($data->type_user); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('id_document')); ?>:</b>
+	<?php echo CHtml::encode($data->id_document); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('email')); ?>:</b>
+	<?php echo CHtml::encode($data->email); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('password')); ?>:</b>
+	<?php echo CHtml::encode($data->password); ?>
+	<br />
+
+	<?php /*
+	<b><?php echo CHtml::encode($data->getAttributeLabel('last_login_time')); ?>:</b>
+	<?php echo CHtml::encode($data->last_login_time); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('create_user_id')); ?>:</b>
+	<?php echo CHtml::encode($data->create_user_id); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('create_time')); ?>:</b>
+	<?php echo CHtml::encode($data->create_time); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('update_user_id')); ?>:</b>
+	<?php echo CHtml::encode($data->update_user_id); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('update_time')); ?>:</b>
+	<?php echo CHtml::encode($data->update_time); ?>
+	<br />
+
+	*/ ?>
+
+</div>
---
+++ dentist/protected/views/user/view.php
@@ -0,0 +1,37 @@
+<?php
+/* @var $this UserController */
+/* @var $model User */
+
+$this->breadcrumbs=array(
+	'Users'=>array('index'),
+	$model->username,
+);
+
+$this->menu=array(
+	array('label'=>'List User', 'url'=>array('index')),
+	array('label'=>'Create User', 'url'=>array('create')),
+	array('label'=>'Update User', 'url'=>array('update', 'id'=>$model->username)),
+	array('label'=>'Delete User', 'url'=>'#', 'linkOptions'=>array('submit'=>array('delete','id'=>$model->username),'confirm'=>'Are you sure you want to delete this item?')),
+	array('label'=>'Manage User', 'url'=>array('admin')),
+);
+?>
+
+<h1>View User #<?php echo $model->username; ?></h1>
+
+<?php $this->widget('zii.widgets.CDetailView', array(
+	'data'=>$model,
+	'attributes'=>array(
+		'username',
+		'type_document',
+		'user_status',
+		'type_user',
+		'id_document',
+		'email',
+		'password',
+		'last_login_time',
+		'create_user_id',
+		'create_time',
+		'update_user_id',
+		'update_time',
+	),
+)); ?>
