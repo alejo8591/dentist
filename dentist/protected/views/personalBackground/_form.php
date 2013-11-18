10198e27e1f4fa15cb04e4af05d4e18c37a7237e
Alejandro Romero <alejo8591@gmail.com>
2013-11-18 02:27:40 -0500

    update with verification error in _form of Personal Background

--- dentist/protected/views/personalBackground/_form.php
+++ dentist/protected/views/personalBackground/_form.php
@@ -28,21 +28,6 @@
 		<?php echo $form->error($model,'id_tbl_anamnesis'); ?>
 	</div>
 	<br />
-  <div id="optionsSocialHabits">
-        <?php
-        foreach($model->optionsSocialHabits as $id => $optionsSocialHabits):
-            $this->renderPartial('_optionssocialhabits', array(
-                'model' => $optionsSocialHabits,
-                'index' => $id,
-                'display' => 'block',
-            ));
-        endforeach;
-        ?>
-  </div> 
-   <div>
-      <?php echo CHtml::link('Antecedentes Psicosociales', '#', array('id' => 'loadOptionsSocialHabitsByAjax')); ?>
-   </div>
-   <br />
 	<div class="row buttons">
 		<?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save'); ?>
 	</div>
@@ -50,43 +35,3 @@
 <?php $this->endWidget(); ?>
 
 </div><!-- form -->
-
-<?php
-   $index = 0;
-    Yii::app()->clientScript->registerScript('loadscripts', '
-    var _index = ' . $index . ';
-    $("#loadOptionsSocialHabitsByAjax").click(function(e){
-       e.preventDefault();
-       var _url = "' . Yii::app()->controller->createUrl("loadOptionsSocialHabitsByAjax", array("load_for_one" => $this->action->id)) . '&index="+_index;
-       $.ajax({
-          url: _url,
-          success:function(response){
-             $("#optionsSocialHabits").append(response);
-             $("#optionsSocialHabits .crow").last().animate({
-                opacity : 1, 
-                 left: "+50", 
-                 height: "toggle"
-             });
-          }
-       });
-    _index++;});
-
-    var __index = ' . $index . ';
-    $("#loadPhoneByAjax").click(function(e){
-       e.preventDefault();
-       var __url = "' . Yii::app()->controller->createUrl("loadPhoneByAjax", array("load_for_two" => $this->action->id)) . '&index="+__index;
-       $.ajax({
-          url: __url,
-          success:function(response){
-             $("#phone").append(response);
-             $("#phone .crow").last().animate({
-                opacity : 1, 
-                 left: "+50", 
-                 height: "toggle"
-             });
-          }
-       });
-    __index++;});
-
-    ', CClientScript::POS_END); 
- ?>
